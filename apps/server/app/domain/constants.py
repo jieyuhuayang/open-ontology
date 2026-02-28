ebe5b4b2508be2bab5c2f5eee984426ad7315a26
LineWalker <jieyuhuayang@foxmail.com>
2026-02-28 21:26:18 +0800

    chore: auto-save constants.py (21:26:18)

---
+++ apps/server/app/domain/constants.py
@@ -0,0 +1,7 @@
+"""MVP default constants (AD-5)."""
+
+DEFAULT_USER_ID = "default"
+DEFAULT_ONTOLOGY_RID = "ri.ontology.ontology.default"
+DEFAULT_PROJECT_RID = "ri.ontology.space.default"
+DEFAULT_PAGE_SIZE = 20
+MAX_PAGE_SIZE = 100
