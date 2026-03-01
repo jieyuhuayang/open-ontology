e17971e56ceb944605a5f8aa02371441a38313c4
LineWalker <jieyuhuayang@foxmail.com>
2026-03-01 15:15:11 +0800

    chore: auto-save i18n.ts (15:15:10)

--- apps/web/src/locales/i18n.ts
+++ apps/web/src/locales/i18n.ts
@@ -2,20 +2,25 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import en from './en/common.json';
-import zh from './zh/common.json';
+import enUS from './en-US/common.json';
+import zhCN from './zh-CN/common.json';
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
-      en: { translation: en },
-      zh: { translation: zh },
+      'en-US': { translation: enUS },
+      'zh-CN': { translation: zhCN },
     },
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false,
+    supportedLngs: ['en-US', 'zh-CN'],
+    load: 'currentOnly',
+    fallbackLng: 'en-US',
+    nonExplicitSupportedLngs: false,
+    interpolation: { escapeValue: false },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
     },
   });
 
