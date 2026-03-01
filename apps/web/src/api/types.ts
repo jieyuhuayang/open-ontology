15b6a89085d7405097a322d0337c6e6ec7c6b268
LineWalker <jieyuhuayang@foxmail.com>
2026-03-02 02:40:48 +0800

    chore: auto-save types.ts (02:40:48)

--- apps/web/src/api/types.ts
+++ apps/web/src/api/types.ts
@@ -8,3 +8,10 @@ export type Icon = components['schemas']['Icon'];
 export type ResourceStatus = components['schemas']['ResourceStatus'];
 export type Visibility = components['schemas']['Visibility'];
 export type ChangeState = components['schemas']['ChangeState'];
+
+export type LinkType = components['schemas']['LinkTypeWithChangeState'];
+export type LinkSide = components['schemas']['LinkSide'];
+export type LinkTypeCreateRequest = components['schemas']['LinkTypeCreateRequest'];
+export type LinkTypeUpdateRequest = components['schemas']['LinkTypeUpdateRequest'];
+export type LinkTypeListResponse = components['schemas']['LinkTypeListResponse'];
+export type Cardinality = components['schemas']['Cardinality'];
