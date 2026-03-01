0e69cf16122fbbd2b3c9fe2875e250256500c24d
LineWalker <jieyuhuayang@foxmail.com>
2026-03-01 21:31:01 +0800

    chore: auto-save types.ts (21:31:01)

---
+++ apps/web/src/api/types.ts
@@ -0,0 +1,10 @@
+import type { components } from '@/generated/api';
+
+export type ObjectType = components['schemas']['ObjectTypeWithChangeState'];
+export type ObjectTypeCreateRequest = components['schemas']['ObjectTypeCreateRequest'];
+export type ObjectTypeUpdateRequest = components['schemas']['ObjectTypeUpdateRequest'];
+export type ObjectTypeListResponse = components['schemas']['ObjectTypeListResponse'];
+export type Icon = components['schemas']['Icon'];
+export type ResourceStatus = components['schemas']['ResourceStatus'];
+export type Visibility = components['schemas']['Visibility'];
+export type ChangeState = components['schemas']['ChangeState'];
