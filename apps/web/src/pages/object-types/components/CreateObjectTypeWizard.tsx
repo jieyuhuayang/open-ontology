b3cb38785fb5df4786bb6695ed5ec09a3868c451
LineWalker <jieyuhuayang@foxmail.com>
2026-03-03 15:10:21 +0800

    chore: auto-save CreateObjectTypeWizard.tsx (15:10:21)

--- apps/web/src/pages/object-types/components/CreateObjectTypeWizard.tsx
+++ apps/web/src/pages/object-types/components/CreateObjectTypeWizard.tsx
@@ -25,7 +25,6 @@ export default function CreateObjectTypeWizard() {
     currentStep,
     selectedDatasetRid,
     displayName,
-    pluralDisplayName,
     description,
     icon,
     properties,
