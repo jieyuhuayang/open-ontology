828bfd37d2043b8bb1d0fb4d4a86384041225570
LineWalker <jieyuhuayang@foxmail.com>
2026-03-04 16:21:24 +0800

    chore: auto-save data-connection-store.ts (16:21:24)

---
+++ apps/web/src/stores/data-connection-store.ts
@@ -0,0 +1,26 @@
+import { create } from 'zustand';
+
+type ActiveTab = 'connections' | 'datasets';
+type ModalType = 'mysqlImport' | 'fileUpload' | null;
+
+interface DataConnectionState {
+  activeTab: ActiveTab;
+  setActiveTab: (tab: ActiveTab) => void;
+  openModal: ModalType;
+  setOpenModal: (modal: ModalType) => void;
+  selectedConnectionRid: string | null;
+  setSelectedConnectionRid: (rid: string | null) => void;
+  previewDatasetRid: string | null;
+  setPreviewDatasetRid: (rid: string | null) => void;
+}
+
+export const useDataConnectionStore = create<DataConnectionState>((set) => ({
+  activeTab: 'connections',
+  setActiveTab: (tab) => set({ activeTab: tab }),
+  openModal: null,
+  setOpenModal: (modal) => set({ openModal: modal }),
+  selectedConnectionRid: null,
+  setSelectedConnectionRid: (rid) => set({ selectedConnectionRid: rid }),
+  previewDatasetRid: null,
+  setPreviewDatasetRid: (rid) => set({ previewDatasetRid: rid }),
+}));
